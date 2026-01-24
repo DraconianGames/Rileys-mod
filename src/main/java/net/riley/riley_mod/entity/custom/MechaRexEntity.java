848ec59ae8965634b4f3806d7d05ac92393ef5cf
Riley Licht <riley.e.licht@gmail.com>
2026-01-24 17:16:17 -0500

    Mecha Rex AI and animations.

--- src/main/java/net/riley/riley_mod/entity/custom/MechaRexEntity.java
+++ src/main/java/net/riley/riley_mod/entity/custom/MechaRexEntity.java
@@ -66,7 +66,7 @@ public class MechaRexEntity extends TamableAnimal {
                 .add(Attributes.ATTACK_DAMAGE, 20f);
 
     }
-//TODO fix animation speed for walking. Register all animations to model. Add attack goal for the tail swipe and bomb launch.
+//TODO fix animation speed for walking. Register all animations to model. Add attack goal for the tail swipe and bomb launch. Figure out how to make it spawn in the Activaction animation.
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
